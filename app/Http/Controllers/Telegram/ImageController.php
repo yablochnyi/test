e538f3eaf8c932a828819e447fa1743e90e74f40
Artem <artem.yablochnyi@yandex.ru>
2023-03-25 17:42:24 +0100

    update image

--- app/Http/Controllers/Telegram/ImageController.php
+++ app/Http/Controllers/Telegram/ImageController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use OpenAI\Laravel\Facades\OpenAI;
 use OpenAI\Responses\Chat\CreateResponse;
+use Stichoza\GoogleTranslate\GoogleTranslate;
 
 class ImageController extends Controller
 {
@@ -57,16 +58,28 @@ class ImageController extends Controller
 
     public function getAssistantResponse($messages): \OpenAI\Responses\Images\CreateResponse
     {
+        $message = str_replace("нарисуй", "", $messages[0]['content']);
+        $message = $this->translate($message);
         $response = OpenAI::images()->create([
-            'prompt' => $messages[0]['content'],
+            'prompt' => $messages,
             'n' => 1,
-            'size' => '256x256',
+            'size' => '512x512',
             'response_format' => 'url',
         ]);
 
         $response->created;
 
         return $response;
+    }
+
+    public function translate($message)
+    {
+        $tr = new GoogleTranslate(); // Translates to 'en' from auto-detected language by default
+//        $tr->setSource('en'); // Translate from English
+        $tr->setSource(); // Detect language automatically
+        $tr->setTarget('en'); // Translate to Georgian
+        $tr->translate($message);
+        return $tr;
     }
 
     public function saveChatContext($user, Request $request, $messages, $response)
@@ -89,55 +102,4 @@ class ImageController extends Controller
             ]);
         }
     }
-//    public function index(Request $request)
-//    {
-//        $this->typing($request);
-//
-//       $response = $this->createImage($request);
-//
-//       $this->saveChatContext($response)
-//
-//
-//
-//        foreach ($response->data as $data) {
-//
-//            Http::post('https://api.tlgr.org/bot6265500701:AAEE7RplIj_t567pNCbFQk9O1xyCBSX7Yng/sendMessage', [
-//                'chat_id' => $request->input('message')['from']['id'],
-//                'text' => $data->url
-//            ]);
-//        }
-//    }
-//
-//    public function typing(Request $request)
-//    {
-//        Http::post('https://api.tlgr.org/bot6265500701:AAEE7RplIj_t567pNCbFQk9O1xyCBSX7Yng/sendChatAction', [
-//            'chat_id' => $request->input('message')['from']['id'],
-//            'action' => 'typing'
-//        ]);
-//    }
-//    public function createImage(Request $request)
-//    {
-//        $response = OpenAI::images()->create([
-//            'prompt' => 'blond',
-//            'n' => 1,
-//            'size' => '256x256',
-//            'response_format' => 'url',
-//        ]);
-//
-//        $response->created;
-//
-//        return $response;
-//    }
-//
-//    public function saveChatContext($user, Request $request, $messages, $response)
-//    {
-//        foreach ($response->data as $data) {
-//            Chat::create([
-//                'telegram_id' => $request->input('message')['from']['id'],
-//                'user_id' => $user->id,
-//                'context' => array_merge($messages, [['role' => 'assistant', 'content' => $data->url]])
-//            ]);
-//        }
-//
-//    }
 }
